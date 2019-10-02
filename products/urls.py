46ec321c555b2c2519f656c801829c9a554118aa
illikkal <bcashahid@gmail.com>
2019-10-02 12:18:43 +0530

    Added files and changes required for deployment to heroku

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from .import views
 from django.views import generic
 urlpatterns=[
-    path('index',views.index,name='index'),
+    path('',views.index,name='index'),
     path('add_product',views.add_product,name='add_product'),
     path('products',views.products,name='products'),
     path('Products/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
